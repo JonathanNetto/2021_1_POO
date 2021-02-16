84bebcbfc4f529d2145127617301f6c330944a8f
Jonathan Netto <jonathan.simplicio@escolar.ifrn.edu.br>
2021-02-16 14:32:31 +0000

    Updating 7e-uri_cedulas-1018.cs

--- Lista_1/7e-uri_cedulas-1018.cs
+++ Lista_1/7e-uri_cedulas-1018.cs
@@ -3,12 +3,14 @@ using System;
 class Cedulas {
   public static void Main (string[] args) {
     int valor = int.Parse(Console.ReadLine());
-    int[] notas = new int[7] {100, 50, 20, 10, 5, 2, 1};
-    
-    //while (c < 7)
-    //  Console.WriteLine($"{q} nota(s) de R$ {}")
-    //List<Valores> lista = new List<Valores>();
-    //lista.Add(100);
-    //Console.WriteLine(lista);
+    int[] Notas = new int[7] {100, 50, 20, 10, 5, 2, 1};
+
+    Console.WriteLine(valor);
+    foreach (int nota in Notas) 
+    {
+      int qua_notas = valor/nota;
+      Console.WriteLine($"{qua_notas} nota(s) de R$ {nota},00");
+      valor -= qua_notas * nota; 
+    }
   }
 }
