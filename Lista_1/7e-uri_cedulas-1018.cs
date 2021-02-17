f488d621e3da0a116eac2bf0742901026aa9d70c
Jonathan Netto <jonathan.simplicio@escolar.ifrn.edu.br>
2021-02-17 19:26:34 +0000

    Add Cédulas

--- Lista_1/7e-uri_cedulas-1018.cs
+++ Lista_1/7e-uri_cedulas-1018.cs
@@ -6,6 +6,7 @@ class Cedulas {
     int[] Notas = new int[7] {100, 50, 20, 10, 5, 2, 1};
 
     Console.WriteLine(valor);
+    
     foreach (int nota in Notas) 
     {
       int qua_notas = valor/nota;
