c01b7c737caa5edfa90b553527f20eca55f5763d
Jonathan Netto <jonathan.simplicio@escolar.ifrn.edu.br>
2021-02-17 19:23:36 +0000

    Add O Maior

--- Lista_1/7b-uri_o_maior-1013.cs
+++ Lista_1/7b-uri_o_maior-1013.cs
@@ -1,8 +1,7 @@
 using System;
 
 class Maior {
-  public static void Main (string[] args) {
-    
+  public static void Main (string[] args) { 
     string [] vet = Console.ReadLine().Split(' ');
     int a = int.Parse(vet[0]);
     int b = int.Parse(vet[1]);
