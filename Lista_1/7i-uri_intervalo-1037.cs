1c4190ff054b5914b3eac362c59f73f2fade8500
Jonathan Netto <jonathan.simplicio@escolar.ifrn.edu.br>
2021-02-18 19:40:33 +0000

    Add Intervalo

--- Lista_1/7i-uri_intervalo-1037.cs
+++ Lista_1/7i-uri_intervalo-1037.cs
@@ -5,28 +5,18 @@ class Intervalo {
     double v = double.Parse(Console.ReadLine());
 
       if (0 <= v && v <= 25)
-      {
         Console.WriteLine("Intervalo [0,25]");
-      }
 
       else if (25 < v && v <= 50)
-      {
         Console.WriteLine("Intervalo (25,50]");
-      }
 
       else if (50 < v && v <= 75)
-      {
         Console.WriteLine("Intervalo (50,75]");
-      }
 
       else if (75 < v && v <= 100)
-      {
         Console.WriteLine("Intervalo (75,100]");
-      }
       
       else
-      {
         Console.WriteLine("Fora de intervalo");
-      }
   }
 }
