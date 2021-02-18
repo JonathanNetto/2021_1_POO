92563c459b927ef916d80f810cff9c348039dd24
Jonathan Netto <jonathan.simplicio@escolar.ifrn.edu.br>
2021-02-18 19:43:26 +0000

    Add Teste de Seleção 1

--- Lista_1/7h-uri_teste_de_selecao1-1035.cs
+++ Lista_1/7h-uri_teste_de_selecao1-1035.cs
@@ -9,12 +9,8 @@ class Selecao {
     int d = int.Parse(abcd[3]);
 
     if ((b > c) && (d > a) && (c+d > a+b) && (c>0 && d>0) && (a%2 == 0))
-    {
       Console.WriteLine("Valores aceitos");
-    }
     else
-    {
       Console.WriteLine("Valores nao aceitos");
-    }
   }
 }
